6c891e3f7cf6dfa14049e78c137389ad25e43269
Michał Góral <virgoerns@gmail.com>
2011-11-20 02:05:54 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+__AUTHOR__ = "Michał Góral"
+
+setup(
+	name = "subconvert",
+	description = "Movie subtitles converter",
+	author = __AUTHOR__,
+	author_email = "michal.goral@mgoral.org",
+	url = "https://github.com/virgoerns/subconvert",
+	license = "GPLv3+",
+	scripts = ["subconvert"]
+)
+
