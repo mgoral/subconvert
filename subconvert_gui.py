17e37cad8ec1f9b049c9a0e91054da3242fe8f87
Michał Góral <virgoerns@gmail.com>
2011-11-22 17:10:18 +0100

    Open files.

--- subconvert_gui.py
+++ subconvert_gui.py
@@ -22,11 +22,16 @@ import sys
 from PyQt4 import QtGui
 import pkgutil
 import encodings
+from subprocess import Popen, PIPE
+import logging
 import gettext
 import subconvert
 
 __VERSION__ = '0.1'
 __AUTHOR__ = u'Michał Góral'
+
+log = logging.getLogger(__name__)
+ch = logging.StreamHandler()
 
 gettext.bindtextdomain('subconvert', '/usr/lib/subconvert/locale')
 gettext.textdomain('subconvert')
@@ -61,7 +66,9 @@ class SubConvertGUI(QtGui.QWidget):
 
 		self.grid.setSpacing(10)
 		self.encodings.addItems(self.get_encodings())
-		self.output_formats.addItems(self.get_formats())
+		#self.output_formats.addItems(self.get_formats())
+		for fmt in self.get_formats():
+			self.output_formats.addItem(fmt[0], fmt[1])
 		self.output_extensions.addItems(self.get_extensions())
 		self.fps.addItems(['23.976', '24', '25', '29.97', '30'])
 
@@ -69,6 +76,7 @@ class SubConvertGUI(QtGui.QWidget):
 		self.add_movie_file.clicked.connect(self.open_dialog)
 		self.remove_file.clicked.connect(self.remove_from_list)
 		self.auto_fps.clicked.connect(self.change_auto_fps)
+		self.start.clicked.connect(self.convert_files)
 		
 		self.grid.addWidget(self.encoding_label, 0, 0)
 		self.grid.addWidget(self.encodings, 0, 1)
@@ -101,10 +109,8 @@ class SubConvertGUI(QtGui.QWidget):
 
 	def get_formats(self):
 		cls = subconvert.GenericSubParser.__subclasses__()
-		formats = set([ "%s (%s)" % (c.__OPT__, c.__SUB_TYPE__) for c in cls ])
-		formats = list(formats)
-		formats.sort()
-		return formats
+		for c in cls:
+			yield (c.__SUB_TYPE__, c.__OPT__)
 
 	def get_extensions(self):
 		cls = subconvert.GenericSubParser.__subclasses__()
@@ -135,7 +141,42 @@ class SubConvertGUI(QtGui.QWidget):
 		else:
 			self.fps.setEnabled(True)
 
+	def convert_files(self):
+		fps = str(self.fps.currentText())
+		encoding = str(self.encodings.currentText())
+		movie_file = str(self.movie_path.text())
+		files = [str(self.file_list.item(i).text()) for i in xrange(self.file_list.count())]
+		sub_format = str(self.output_formats.itemData(self.output_formats.currentIndex()).toString())
+		extension = str(self.output_extensions.itemText(self.output_extensions.currentIndex())) if self.output_extensions.currentIndex() > 0 else ''
+
+		for arg in files:
+			# Call it 'arg' to keep a consistency with cli version
+			if self.auto_fps.isChecked():
+				exts = ('.avi', '.mkv', '.mpg', '.mp4', '.wmv')
+				if not movie_file:
+					filename, extension = os.path.splitext(arg)
+					for ext in exts:
+						f = ''.join((filename, ext))
+						if os.path.isfile(f):
+							fps = subconvert.mplayer_check(f, fps)
+							break
+				else:
+					fps = subconvert.mplayer_check(movie_file, fps)
+
+			
+
+			try:
+				conv, lines = subconvert.convert_file(arg, encoding, fps, sub_format, extension)
+			except NameError:
+				log.error(_("'%s' format not supported (or mistyped).") % sub_format)
+				return -1
+			except UnicodeDecodeError:
+				log.error(_("Couldn't handle '%s' given '%s' encoding.") % (arg, encoding))
+				continue
+
 def main():
+	log.setLevel(logging.INFO)
+	log.addHandler(ch)
 	app = QtGui.QApplication(sys.argv)
 	gui = SubConvertGUI()
 	sys.exit(app.exec_())
