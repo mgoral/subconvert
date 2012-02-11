0b976a0001b4cd0055504ea230c5b367f168c50c
Michał Góral <virgoerns@gmail.com>
2012-02-11 23:48:22 +0100

    Updated usage print.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function printUsage {
     echo "Usage:"
-    echo "   install.sh install | uninstall | reinstall"
+    echo "   install.sh install|uninstall|reinstall [install_path]"
 }
 
 function remove {
